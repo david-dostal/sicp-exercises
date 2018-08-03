592845572819c6fc41c6d07377eef22fc5f25f5a
David Dostal <david.dostal1112@gmail.com>
2018-08-03 16:02:07 +0200

    Rename 'curious questions' to 'experiments'

--- 1.1 - The elements of programming/experiments.scm
+++ 1.1 - The elements of programming/experiments.scm
@@ -1,18 +1,15 @@
 #lang sicp
 
-; Curious Questions:
-; ==================
-
-; 1)
 ; What does a procedure evaluete to? What is printed as output?
+; =============================================================
 
 (define (f) 2)
 (define (g x) x)
 (g f)
 ; #<procedure:f>
 
-; 2)
 ; What happens if no conditions match?
+; ====================================
 
 (define (test)
   (cond (false 1)
@@ -28,8 +25,8 @@
 ; error, given #<void>
 
 
-; 3)
 ; Can we define a procedure conditionally?
+; ========================================
 
 (if true
     (define a 1)
