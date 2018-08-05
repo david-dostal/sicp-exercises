3b826ff6b21f8edf03b0b3e8c9170ad8c858ecdd
David Dostal <david.dostal1112@gmail.com>
2018-08-05 14:38:38 +0200

    Add exercise 1.14

--- 1.2 - Procedures and the Processes They Generate/count-change.scm
+++ 1.2 - Procedures and the Processes They Generate/count-change.scm
@@ -5,16 +5,16 @@
 ; ================
 
 (define (count-change amt)
-  (define (count-iter amt max-coin)
+  (define (cc amt max-coin)
     (cond ((< amt 0) 0)      ; no way to get an amount < 0
           ((< max-coin 0) 0) ; no coins left
           ((= amt 0) 1)      ; zero coins = amt 0 (1 way)
           (else (+
                  ; choose coin and count ways to arrange the rest
-                 (count-iter (- amt (coins max-coin)) max-coin)
+                 (cc (- amt (coins max-coin)) max-coin)
                  ; or we can try a different coin with the initial amount
-                 (count-iter amt (- max-coin 1))))))
-  (count-iter amt 4))
+                 (cc amt (- max-coin 1))))))
+  (cc amt 4))
 
 (define (coins n)
   (cond ((= n 0) 1)
