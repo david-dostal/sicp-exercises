2a55e27d9423610bb6b309f7c5aac72601bbf2f9
David Dostal <david.dostal1112@gmail.com>
2018-08-27 18:42:28 +0200

    Add fast-prime procedure

---
+++ 1.2 - Procedures and the Processes They Generate/fast-prime.scm
@@ -0,0 +1,28 @@
+#lang sicp
+(#%require "square.scm")
+(#%require (lib "27.ss" "srfi")) ; for getting large random integers
+(#%provide (all-defined))
+
+; Fast prime (Fermat test)
+; ========================
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder
+          (square (expmod base (/ exp 2) m))
+          m))
+        (else
+         (remainder
+          (* base (expmod base (- exp 1) m))
+          m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random-integer (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
