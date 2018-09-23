72e24fc23deb2b74235ed054ec25f41be882aa09
David Dostal <david.dostal1112@gmail.com>
2018-09-23 13:04:29 +0200

    Add exercise 2.12

---
+++ 2.1 - Introduction to Data Abstraction/2.12.scm
@@ -0,0 +1,29 @@
+#lang sicp
+(#%require "2.07.scm")
+(#%require "2.08.scm")
+
+; Exercise 2.12:
+; ==============
+; Define a constructor make-center-percent that takes a center and a percentage
+; tolerance and produces the desired interval. You must also define a selector
+; percent that produces the percentage tolerance for a given interval.
+; The center selector is the same as the one shown above.
+
+; The code shown above:
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+
+
+; Solution to the exercise:
+(define (make-center-percent c p)
+  (make-center-width c (* c (/ p 100.0))))
+
+(define (percent i)
+  (* 100.0 (/ (width i) (center i))))
